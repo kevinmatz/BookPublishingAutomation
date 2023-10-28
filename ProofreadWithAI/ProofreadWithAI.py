5e0582a1cfccd8f20fb9279126a4636f71cffd0f
Kevin Matz <kevin@winchelseasystems.com>
2023-10-28 13:35:16 -0700

    Fixing handling of paragraph breaks in the Markdown content (prior to this fix, it was generating a wall of text with no paragraph breaks at all)

--- ProofreadWithAI/ProofreadWithAI.py
+++ ProofreadWithAI/ProofreadWithAI.py
@@ -32,41 +32,46 @@ with open(source_filename, "rb") as docx_file:
     result = mammoth.convert_to_markdown(docx_file)
     md_text = result.value
 
-# Break the markdown text into segments
-segments = textwrap.wrap(md_text, 2048)
+# Break the markdown text into paragraphs
+paragraphs = md_text.split('\n\n')
 
-# Proofread each segment
+# Proofread each paragraph
 segments_count = 0
 proofread_segments = []
 prompt_format = "Please perform copy editing on this Markdown text to correct spelling, grammar, usage, and punctuation mistakes, and to ensure consistent usage:\n {}"
 
-for segment in segments:
-    segments_count = segments_count + 1
+for paragraph in paragraphs:
+    # Break the paragraph into segments
+    segments = textwrap.wrap(paragraph, 2048)
+    for segment in segments:
+        segments_count = segments_count + 1
 
-    print("Segment {x}:".format(x=segments_count))
-    print("-----")
-    print(segment)
-    print("-----")
+        print("Segment {x}:".format(x=segments_count))
+        print("-----")
+        print(segment)
+        print("-----")
 
-    """
-        prompt = prompt_format.format(segment)
-        response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=2100
-        )
-        # Save the corrected text
-        proofread_segments.append(response.choices[0].text.strip())
-    """
+        """
+            prompt = prompt_format.format(segment)
+            response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=2100
+            )
+            # Save the corrected text
+            proofread_segments.append(response.choices[0].text.strip())
+        """
 
-    # For now, until we can get openai package working, just keep the segment unchanged
-    proofread_segments.append(segment)
-
-# print("segments_count: ", segments_count)
+        # For now, until we can get openai package working, just keep the segment unchanged
+        proofread_segments.append(segment)
+    
+    # proofread_segments.append("\n\n")
 
 # Reassemble the proofread markdown
-proofread_md = ''.join(proofread_segments)
+proofread_md = '\n\n'.join(proofread_segments)
+
+# print("segments_count: ", segments_count)
 
 # Write the proofread markdown to a file
 # TODO: How to generate a temp filename properly in Python? And can it be auto-deleted at program termination like in Java?
